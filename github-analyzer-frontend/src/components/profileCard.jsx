9370543609d311a715e69d32e97cc136dae0c683
Mr-Thanush <thanush.2645@gmail.com>
2026-06-04 17:56:43 +0530

    removed Consoles

--- github-analyzer-frontend/src/components/profileCard.jsx
+++ github-analyzer-frontend/src/components/profileCard.jsx
@@ -31,7 +31,7 @@ const ProfileCard=()=>{
 }
 
 
-    console.log(currentProfile); 
+    
 
     return(
                 <div
--- github-analyzer-frontend/src/components/profilesList.jsx
+++ github-analyzer-frontend/src/components/profilesList.jsx
@@ -10,7 +10,7 @@ function ProfilesList() {
 
     
 
-    console.log(profiles);
+    
     return (
         <div className='max-w-4xl mx-auto mt-10'>
             <h2 className='text-3xl font-bold text-center mb-4'>
